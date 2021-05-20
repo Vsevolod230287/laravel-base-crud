9f136f537f3743394a06fc1b12f521325e49cc76
Vsevolod <cerneavschivsevolod@gmail.com>
2021-05-20 21:15:25 +0200

    Fatto l'index

---
+++ database/migrations/2021_05_20_135343_create_comics_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb');
+            $table->float('price', 4, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 30);
+            $table->timestamps();
+        });
+    }
+
+
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
